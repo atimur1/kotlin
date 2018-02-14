2873f3481b28a8e3083a0da984b53fc92a6f9d1c
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-02-14 08:07:50 +0300

    Don't try to get value argument directly by parameter descriptor
    Consider the following situations for parameter descriptor:
     - it can be original
     - it can be substituted
     - it can come from candidate descriptor
     - it can come from resulting descriptor
    
    Therefore, it's very dangerous to rely on parameter descriptors `hashCode`/`equals` and better to use only `index` from it

--- idea/src/org/jetbrains/kotlin/idea/slicer/Slicer.kt
+++ idea/src/org/jetbrains/kotlin/idea/slicer/Slicer.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.slicer
@@ -43,6 +32,7 @@ import org.jetbrains.kotlin.cfg.pseudocodeTraverser.TraversalOrder
 import org.jetbrains.kotlin.cfg.pseudocodeTraverser.traverse
 import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithSource
+import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
 import org.jetbrains.kotlin.descriptors.VariableDescriptorWithAccessors
 import org.jetbrains.kotlin.descriptors.impl.SyntheticFieldDescriptor
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
@@ -71,6 +61,7 @@ import org.jetbrains.kotlin.resolve.calls.model.ExpressionValueArgument
 import org.jetbrains.kotlin.resolve.scopes.receivers.ExpressionReceiver
 import org.jetbrains.kotlin.resolve.source.getPsi
 import org.jetbrains.kotlin.util.OperatorNameConventions
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import java.util.*
 
 private fun PsiElement.processHierarchyDownward(scope: SearchScope, processor: PsiElement.() -> Unit) {
@@ -259,7 +250,7 @@ class InflowSlicer(
                     .forEach { (it.element?.parent as? KtProperty)?.processPropertyAssignments() }
         }
 
-        val parameterDescriptor = analyze()[BindingContext.VALUE_PARAMETER, this] ?: return
+        val parameterDescriptor = analyze()[BindingContext.VALUE_PARAMETER, this]?.safeAs<ValueParameterDescriptor>() ?: return
 
         (function as? KtFunction)?.processCalls(parentUsage.scope.toSearchScope()) body@ {
             val refElement = it.element ?: return@body
@@ -269,7 +260,7 @@ class InflowSlicer(
                 refElement is KtExpression -> {
                     val callElement = refElement.getParentOfTypeAndBranch<KtCallElement> { calleeExpression } ?: return@body
                     val resolvedCall = callElement.getResolvedCall(callElement.analyze()) ?: return@body
-                    val resolvedArgument = resolvedCall.valueArguments[parameterDescriptor] ?: return@body
+                    val resolvedArgument = resolvedCall.valueArgumentsByIndex?.get(parameterDescriptor.index) ?: return@body
                     when (resolvedArgument) {
                         is DefaultValueArgument -> defaultValue
                         is ExpressionValueArgument -> resolvedArgument.valueArgument?.getArgumentExpression()
